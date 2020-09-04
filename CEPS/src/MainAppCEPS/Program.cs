65695031738c7562104301c7214e1d2ac9ae8679
Anh Tuan Nguyen <tuan.anh.nguyen@unicornsystems.eu>
2020-09-04 09:45:08 +0200

    Zalozeni projektu CEPS

---
+++ CEPS/src/MainAppCEPS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainAppCEPS
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+
+      CEPSModules1.Class1 cls1 = new CEPSModules1.Class1();
+      cls1.RunTests();
+    }
+  }
+}
