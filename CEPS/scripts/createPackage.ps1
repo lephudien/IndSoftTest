28fe90fec1988e410a6b666bb0aad4b3af77ba17
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-09-07 13:29:03 +0200

    Uprava skriptu

--- CEPS/scripts/createPackage.ps1
+++ CEPS/scripts/createPackage.ps1
@@ -4,6 +4,9 @@ param($PackageVersion)
 #$packageVersion = "1.0.0.10"
 Write-Host "PackageVersion: " $PackageVersion
 
+$PathProjectRoot = (get-item $PSScriptRoot).parent.FullName
+$PathProjectBuildResources = $PathProjectRoot + "\BuildResources"
+
 $PackageOutFolder = $("S:\TeamCity\output\builds\IndSoftCEPS_v" + $PackageVersion)
 Write-Host "PackageOutFolder: " $PackageOutFolder
 New-Item -ItemType Directory -Force -Path $PackageOutFolder
@@ -11,4 +14,4 @@ New-Item -ItemType Directory -Force -Path $PackageOutFolder
 $ZipFile = $($PackageOutFolder + "\IndSoftCEPS_v" + $PackageVersion + ".zip")
 Write-Host "ZipFile: " $ZipFile
 # Create a zip file with the contents of C:\Stuff\
-Compress-Archive -Path 'c:\Program Files (x86)\BuildAgent\work\9a159fa32295444\CEPS\BuildResources' -DestinationPath $ZipFile
+Compress-Archive -Path $PathProjectBuildResources -DestinationPath $ZipFile
